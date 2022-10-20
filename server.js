f0f6b55d9ab79869eda27441c0562b89f3d83454
ValentinLanglet59 <valdu59960@hotmail.fr>
2022-10-20 17:10:37 +0200

    feat : mern network front done

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
 connectDB();
 
-app.use(express.json({ extended: false}))
+app.use(express.json({ extended: false }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.get('/', (req,res,next) => {res.send('hello')})
+app.get('/', (req, res, next) => {
+  res.send('hello');
+});
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {console.log(`server started on ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
+});
